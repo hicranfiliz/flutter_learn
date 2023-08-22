3581092ce26fddecf89c8a74a317e01e51765962
Hicran Filiz <h.filiz781@gmail.com>
2023-08-22 12:00:28 +0300

    added text learn view dart file

---
+++ lib/101/text_learn_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class TextLearnView extends StatelessWidget {
+  TextLearnView({super.key, this.userName});
+  final String name = 'hicran';
+  final String? userName;
+
+  final ProjectKeys keys = ProjectKeys();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            ('Welcome $name ${name.length}'),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: ProjectStyles.welcomeStyle,
+          ),
+          Text(
+            ('Welcome $name ${name.length}'),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: ProjectStyles.welcomeStyle,
+          ),
+          Text(('Welcome $name ${name.length}'),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.right,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    color: ProjectColors.welcomeColor,
+                  )),
+          Text(userName ?? " "),
+          Text(keys.welcome),
+        ],
+      )),
+    );
+  }
+}
+
+class ProjectStyles {
+  static TextStyle welcomeStyle = TextStyle(
+      wordSpacing: 2,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      letterSpacing: 2,
+      color: Colors.lime,
+      fontSize: 16,
+      fontWeight: FontWeight.w600);
+}
+
+class ProjectColors {
+  static Color welcomeColor = Colors.red;
+}
+
+class ProjectKeys {
+  final String welcome = "Merhaba";
+}
