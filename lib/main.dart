9f58fee1ea7de668bb717b593a4c7428e889491f
Hicran Filiz <h.filiz781@gmail.com>
2023-09-02 22:10:48 +0300

    added  models view file

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ import 'package:flutter_learn/101/statefull_lifecycle_learn.dart';
 import 'package:flutter_learn/101/stateless_learn.dart';
 import 'package:flutter_learn/101/text_field_learn.dart';
 import 'package:flutter_learn/101/text_learn_view.dart';
+import 'package:flutter_learn/202/model_learn_view.dart';
 import 'package:flutter_learn/202/tab_learn.dart';
 import 'package:flutter_learn/demos/color_demos_view.dart';
 import 'package:flutter_learn/demos/color_lifecycle_view.dart';
@@ -71,6 +72,6 @@ class MyApp extends StatelessWidget {
               backgroundColor: Colors.transparent,
               elevation: 0,
             )),
-        home: const TabLearn());
+        home: const ModelLearnView());
   }
 }
