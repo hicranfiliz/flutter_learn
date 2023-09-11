3d125fbd58b2664a226d7d4636609742aabc9c8b
Hicran Filiz <h.filiz781@gmail.com>
2023-09-11 11:37:13 +0300

    image learn 202 with extensions

---
+++ lib/202/image_learn202.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ImageLearn202 extends StatefulWidget {
+  const ImageLearn202({super.key});
+
+  @override
+  State<ImageLearn202> createState() => _ImageLearn202State();
+}
+
+class _ImageLearn202State extends State<ImageLearn202> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ImagePaths.dal.toWidget(height: 100));
+  }
+}
+
+enum ImagePaths { dal }
+
+// Best Practice kullanim!
+extension ImagePathsExtension on ImagePaths {
+  String path() {
+    return 'assets/png/$name.png';
+  }
+
+  Widget toWidget({double height = 24}) {
+    return Image.asset(path(), height: height);
+  }
+}
