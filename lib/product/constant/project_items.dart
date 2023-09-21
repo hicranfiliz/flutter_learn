3fa8fbd458d8c33e96b86a168df4afbea9cc230b
hicranfiliz <h.filiz781@gmail.com>
2023-09-21 23:39:34 +0300

    added network manager for singleton

--- lib/product/constant/project_items.dart
+++ lib/product/constant/project_items.dart
@@ -1,5 +1,25 @@
 
 
+import 'package:dio/dio.dart';
+
 class ProjectItems{
+  ProjectItems._();
   static const String projectName = 'HF10';
+  // projectitems'dan yeni bir instance uretilmesini engeller.
+  
 }
+
+// ama network manager katmani yaparken boyle kullanmak cok mantikli degil.
+
+// class ProjectNetworkManager{
+//    ProjectNetworkManager._(){
+//     _dio = Dio(BaseOptions(baseUrl: 'pub.dev'));
+//   }
+
+//    late final Dio _dio;
+
+//    static ProjectNetworkManager instance = ProjectNetworkManager._();
+
+//    Dio get service => _dio;
+
+// }
